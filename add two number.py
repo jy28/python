448ce6946b05a83731b38d289cc2d646f3f8e21c
jy28 <jyotisharmaa900@gmail.com>
2018-02-05 11:08:18 +0530

    hello

---
+++ add two number.py
@@ -0,0 +1,4 @@
+num = 3
+num = 7
+sum = num1+num2
+print(sum)
---
+++ calculator.py
@@ -0,0 +1,17 @@
+Python 3.6.4 (v3.6.4:d48eceb, Dec 19 2017, 06:04:45) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+==================== RESTART: E:/python/add two number.py ====================
+Traceback (most recent call last):
+  File "E:/python/add two number.py", line 3, in <module>
+    sum = num1+num2
+NameError: name 'num1' is not defined
+>>> 2 + 2
+4
+>>> 50 - 50*6
+-250
+>>> 20/6
+3.3333333333333335
+>>> (50 -50*6) / 6
+-41.666666666666664
+>>> 
