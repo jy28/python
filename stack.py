c8b9c7a6d6ab06827c85510c5ed37b14998816d8
jy28 <jyotisharmaa900@gmail.com>
2018-02-06 12:24:24 +0530

    python program

---
+++ stack.py
@@ -0,0 +1,15 @@
+#STACK
+stack=[3,4,5]
+stack.append(6)
+stack.append(7)
+stack
+
+
+
+#QUEUE
+
+from collections import deque
+queue=deque(["Eric","John","Micheal"])
+queue.append("Terry")
+queue.append("Graham")
+queue.popleft()
